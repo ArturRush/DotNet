e8ab9bf653e447f59e9b3e817f7224cce54a4669
ArturRush <arturrush@rambler.ru>
2016-10-23 18:07:35 +0500

    Сделана 9 лаба
    Добавлен тестовый проект с подключенной либой
    Добавлен NuGet-пакет (доступен в менеджере)

---
+++ AviationDllTest/AviationDllTest/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aviation;
+using Aviation.Factories;
+
+namespace AviationDllTest
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			RussianAviationFactory raf = new RussianAviationFactory();
+			var hel = raf.CreateHelicopter();
+			hel.MakeFlight(Routs.Cities.Moscow, Routs.Cities.Omsk);
+			Console.ReadKey();
+		}
+	}
+}

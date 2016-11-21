320f6e659a0f1d041164d269e4bd4631776791e5
ArturRush <arturrush@rambler.ru>
2016-11-21 22:16:58 +0500

    Допилил 10 лабу

---
+++ Aviation/AviationExtentions/AviationExtentions/Program.cs
@@ -0,0 +1,148 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Aviation.Aviation;
+using Aviation.Engines;
+using Aviation.Factories;
+using Newtonsoft.Json;
+
+namespace AviationExtentions
+{
+	class Program
+	{
+		private static void FillAer(Aeroport<IPassengerAviation<IEngine>> aer, int planesCount, int heliCount)
+		{
+			AmericanAviationFactory aaf = new AmericanAviationFactory();
+			RussianAviationFactory raf = new RussianAviationFactory();
+
+			Random r = new Random();
+			int americanPlanes = r.Next(planesCount);
+			int russianHelicopters = r.Next(heliCount);
+
+			for (int i = 0; i < planesCount - americanPlanes; ++i)
+			{
+				int reactive = r.Next(2);
+				if (reactive == 1)
+					aer.Add(raf.CreateReactivePlane());
+				else
+					aer.Add(raf.CreateTurbopropPlane());
+			}
+
+			for (int i = 0; i < americanPlanes; ++i)
+			{
+				int reactive = r.Next(2);
+				if (reactive == 1)
+					aer.Add(aaf.CreateReactivePlane());
+				else
+					aer.Add(aaf.CreateTurbopropPlane());
+			}
+
+			for (int i = 0; i < russianHelicopters; ++i)
+			{
+				aer.Add(raf.CreateHelicopter());
+			}
+
+			for (int i = 0; i < heliCount - russianHelicopters; ++i)
+			{
+				aer.Add(aaf.CreateHelicopter());
+			}
+		}
+
+		public static void Main(string[] args)
+		{
+			var aer = new Aeroport<IPassengerAviation<IEngine>>("Кольцово");
+			FillAer(aer, 15, 10);
+			Random r = new Random();
+			foreach (var avia in aer)
+			{
+				avia.PlacePassenger(r.Next(0, avia.Capacity));
+			}
+
+			Console.WriteLine(aer.ConvertToString());
+			var tmpAvia = aer.FindByFreePlaces(3);
+			if (tmpAvia != null)
+				Console.WriteLine("В этом судне достаточно мест:\n" + tmpAvia.ConvertToString());
+			Console.WriteLine(aer.FindAllEmpty().ConvertToString());
+			Console.ReadKey();
+		}
+	}
+
+	/// <summary>
+	/// Класс расширение
+	/// </summary>
+	public static class AeroportExtension
+	{
+		/// <summary>
+		/// Метод перевода коллекции в json-строку
+		/// </summary>
+		/// <typeparam name="T">Тип обобощенной коллекции</typeparam>
+		/// <param name="aer">Коллекция</param>
+		/// <returns>json-строка с объектами коллекции</returns>
+		public static string ConvertToString<T>(this Aeroport<T> aer) where T : IPassengerAviation<IEngine>
+		{
+			ExtensionLogger.Log("Вызван метод конвертации коллекции в строку");
+			return JsonConvert.SerializeObject(aer, Formatting.Indented);
+		}
+
+		/// <summary>
+		/// Находит первое вхождение судна, у которого имеется достаточно свободных мест
+		/// </summary>
+		/// <param name="aer">Коллекция</param>
+		/// <returns>Судно, либо null, если таковых не найдено</returns>
+		public static IPassengerAviation<IEngine> FindByFreePlaces(this Aeroport<IPassengerAviation<IEngine>> aer, int places)
+		{
+			ExtensionLogger.Log("Вызван метод поиска судна с достаточным количеством мест");
+			foreach (var aero in aer)
+			{
+				if (aero.Capacity - aero.Engaged > places)
+					return aero;
+			}
+			ExtensionLogger.Log("Нет судна с достаточным количеством мест");
+			return null;
+		}
+
+		/// <summary>
+		/// Возвращает все пустые воздушные судна из коллекции
+		/// </summary>
+		/// <typeparam name="T">тип обобщенной коллекции</typeparam>
+		/// <param name="aer">Коллекция пустых самолетов</param>
+		/// <returns></returns>
+		public static Aeroport<T> FindAllEmpty<T>(this Aeroport<T> aer) where T : IPassengerAviation<IEngine>
+		{
+			ExtensionLogger.Log("Вызван метод поиска всех пустых суден");
+			var res = new Aeroport<T>(aer.Name);
+			foreach (var aero in aer)
+			{
+				if (aero.Engaged == 0)
+					res.Add(aero);
+			}
+			return res;
+		}
+
+		public static string ConvertToString(this IPassengerAviation<IEngine> avia)
+		{
+			return JsonConvert.SerializeObject(avia, Formatting.Indented);
+		}
+	}
+
+	/// <summary>
+	/// Логгер
+	/// </summary>
+	public static class ExtensionLogger
+	{
+		public static string filePath = "";
+
+		public static void Log(string mes)
+		{
+			if (filePath == "")
+				Console.WriteLine(DateTime.Now + " : " + mes);
+			else
+			{
+				File.AppendAllText(DateTime.Now + " : " + filePath, mes);
+			}
+		}
+	}
+}
---
+++ Aviation/AviationExtentions/AviationExtentions/Properties/AssemblyInfo.cs
@@ -0,0 +1,36 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// Управление общими сведениями о сборке осуществляется с помощью 
+// набора атрибутов. Измените значения этих атрибутов, чтобы изменить сведения,
+// связанные со сборкой.
+[assembly: AssemblyTitle("AviationExtentions")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AviationExtentions")]
+[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Параметр ComVisible со значением FALSE делает типы в сборке невидимыми 
+// для COM-компонентов.  Если требуется обратиться к типу в этой сборке через 
+// COM, задайте атрибуту ComVisible значение TRUE для этого типа.
+[assembly: ComVisible(false)]
+
+// Следующий GUID служит для идентификации библиотеки типов, если этот проект будет видимым для COM
+[assembly: Guid("102191a3-fe94-44c4-bf0d-aebaf0753487")]
+
+// Сведения о версии сборки состоят из следующих четырех значений:
+//
+//      Основной номер версии
+//      Дополнительный номер версии 
+//   Номер сборки
+//      Редакция
+//
+// Можно задать все значения или принять номера сборки и редакции по умолчанию 
+// используя "*", как показано ниже:
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
